d502b763125a74bea8ed63c4d48a28ea64729759
Anastasiia Generalova <rhinrei@gmail.com>
2020-01-07 03:27:39 +0300

    add tests for basket page

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.basket_page import BasketPage
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
@@ -16,3 +17,11 @@ def test_guest_should_see_login_link(browser):
     page.open()
     page.should_be_login_link()
 
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page = BasketPage(browser, browser.current_url)
+    page.should_be_no_items_in_basket()
+    page.should_be_text_empty_basket()
